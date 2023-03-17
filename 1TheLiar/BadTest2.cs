ce1d1d544664a8d2586bffe3edc35291db696a9f
Stephany Henrique Batista <stephany.batista@outlook.com>
2023-03-17 17:04:50 -0400

    Removed some tests to improve the explanation about the problem

--- 1TheLiar/BadTest2.cs
+++ 1TheLiar/BadTest2.cs
@@ -5,24 +5,6 @@ namespace ImprovingOurTestTalk._1TheLiar;
 public class BadTest2
 {
     [Fact]
-    public void Must_validate_Name()
-    {
-        var employee = new Employee(null, 19, "123434");
-        
-        Assert.False(employee.IsValid());
-        Assert.Single(employee.Errors);
-    }
-    
-    [Fact]
-    public void Must_validate_Age()
-    {
-        var employee = new Employee("Henrique Batista", 17, "123434");
-        
-        Assert.False(employee.IsValid());
-        Assert.Single(employee.Errors);
-    }
-    
-    [Fact]
     public void Must_validate_SocialNumber()
     {
         var employee = new Employee("Henrique Batista", 68, string.Empty);
